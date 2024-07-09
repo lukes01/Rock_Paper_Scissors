555eaedb8e1708b87b39242af90d9e89ce22c329
Lucas Stein <stein.lucas2001@gmail.com>
2024-07-09 11:59:49 -0400

    User chooses their item.

--- main.c
+++ main.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 
 int main() {
-    int play = -1;
+    int play = -1;          //boolean val to play or not
+    int user = 0;           //User choice
+    printf("\n\nWould you like to play Rock Paper Scissors?");
+    printf("\nEnter [1] for Yes and [0] for No:\t");
+    scanf("%d", &play);     //Takes user input to play or not
 
-    // while ((play != 1) && (play != 0))
-    // {
-        printf("\n\nWould you like to play Rock Paper Scissors?");
-        printf("\nEnter '1' for Yes and '0' for No.\n");
-        scanf("%d", &play);
-
-        if (play == 0)
-        {
-            printf("Goodbye");
-        }
+    if (play == 1)      //Begins game
+    {
+        printf("\n\nEnter a number to choose.\n[1] for Rock\n"
+                "[2] for Paper\n[3] for Scissors:\t");
+        scanf("%d", &user);
+        printf("You chose %d", user);
+    }
+    else if (play == 0) //Says bye to user if they don't want to play
+    {
+        printf("Goodbye");
+    }
     return 0;
 }
