e7199559143476419137c0243e9cfdc208c49e6e
Lucas Stein <stein.lucas2001@gmail.com>
2024-07-09 11:35:51 -0400

    Ask the user to play

--- main.c
+++ main.c
@@ -3,10 +3,6 @@
 
 int main() {
     int play;
-    int wins = 0;
-    int loses = 0;
-    int ties = 0;
-    int user = 0;
     printf("\n\nWould you like to play Rock Paper Scissors?");
     printf("\nType '1' for Yes and '0' for No.\n");
     scanf("%d", play);
